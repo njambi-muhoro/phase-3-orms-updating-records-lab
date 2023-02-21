07050ef4b52ae0402cce8818f760ae423c6b10f6
+ <+>
2023-02-21 23:39:53 +0300

    Assignment DOne

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,71 @@
 require_relative "../config/environment.rb"
-
 class Student
+  attr_accessor  :name, :grade, :id
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  def initialize(id=nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
+ 
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
 
 
+#this method will wrap the code we used above in the save method of our 
+#attributes that we initialized
+ #to create a new Student instance and save it.
+# Here, we use keyword arguments to pass a name and grade into our
+#.create method. We use that name and grade to instantiate a new student.
+#Then, we use the #save method to persist that student to the database.
+  def self.create(name, grade)
+    student = self.new(name, grade)
+    student.save
+    student
+  end
+
+  def self.new_from_db(row)
+    self.new(row[0], row[1], row[2])
+  end
+
+
+# to do away with shovel <<-SQL you just need to write the select message inside an double quotes
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ? LIMIT 1"
+    row = DB[:conn].execute(sql, name).first
+    self.new_from_db(row) if row
+  end
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 end
